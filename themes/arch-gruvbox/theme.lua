541f37173e4b6340b02072611f7a76a90b4b768a
Rokas Puzonas <rokas.puz@gmail.com>
2022-01-24 00:14:38 +0200

    refactor: move theme icons to shared location

--- themes/arch-gruvbox/theme.lua
+++ themes/arch-gruvbox/theme.lua
@@ -6,6 +6,7 @@ local dpi = xresources.apply_dpi
 local gfs = require("gears.filesystem")
 local assets_path = gfs.get_configuration_dir().."/assets/"
 local theme_path = gfs.get_configuration_dir().."/themes/arch-gruvbox/"
+local icons_path =  assets_path.."/icons/"
 
 local theme = {}
 
@@ -79,7 +80,7 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = theme_path.."submenu.png"
+theme.menu_submenu_icon = icons_path.."submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(110)
 
@@ -89,65 +90,65 @@ theme.menu_width  = dpi(110)
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = theme_path.."titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = theme_path.."titlebar/close_focus.png"
+theme.titlebar_close_button_normal = icons_path.."titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = icons_path.."titlebar/close_focus.png"
 
-theme.titlebar_minimize_button_normal = theme_path.."titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = theme_path.."titlebar/minimize_focus.png"
+theme.titlebar_minimize_button_normal = icons_path.."titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = icons_path.."titlebar/minimize_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = theme_path.."titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = theme_path.."titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = theme_path.."titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = theme_path.."titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = icons_path.."titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = icons_path.."titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = icons_path.."titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = icons_path.."titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = theme_path.."titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = theme_path.."titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = theme_path.."titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = theme_path.."titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = icons_path.."titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = icons_path.."titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = icons_path.."titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = icons_path.."titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = theme_path.."titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = theme_path.."titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = theme_path.."titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = theme_path.."titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = icons_path.."titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = icons_path.."titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = icons_path.."titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = icons_path.."titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = theme_path.."titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = theme_path.."titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = theme_path.."titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = theme_path.."titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = icons_path.."titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = icons_path.."titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = icons_path.."titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = icons_path.."titlebar/maximized_focus_active.png"
 
 theme.wallpaper = theme_path.."wallpaper.png"
 theme.wallpaper_bg = "#272827"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = theme_path.."layouts/fairhw.png"
-theme.layout_fairv = theme_path.."layouts/fairvw.png"
-theme.layout_floating  = theme_path.."layouts/floatingw.png"
-theme.layout_magnifier = theme_path.."layouts/magnifierw.png"
-theme.layout_max = theme_path.."layouts/maxw.png"
-theme.layout_fullscreen = theme_path.."layouts/fullscreenw.png"
-theme.layout_tilebottom = theme_path.."layouts/tilebottomw.png"
-theme.layout_tileleft   = theme_path.."layouts/tileleftw.png"
-theme.layout_tile = theme_path.."layouts/tilew.png"
-theme.layout_tiletop = theme_path.."layouts/tiletopw.png"
-theme.layout_spiral  = theme_path.."layouts/spiralw.png"
-theme.layout_dwindle = theme_path.."layouts/dwindlew.png"
-theme.layout_cornernw = theme_path.."layouts/cornernww.png"
-theme.layout_cornerne = theme_path.."layouts/cornernew.png"
-theme.layout_cornersw = theme_path.."layouts/cornersww.png"
-theme.layout_cornerse = theme_path.."layouts/cornersew.png"
+theme.layout_fairh = icons_path.."layouts/fairhw.png"
+theme.layout_fairv = icons_path.."layouts/fairvw.png"
+theme.layout_floating  = icons_path.."layouts/floatingw.png"
+theme.layout_magnifier = icons_path.."layouts/magnifierw.png"
+theme.layout_max = icons_path.."layouts/maxw.png"
+theme.layout_fullscreen = icons_path.."layouts/fullscreenw.png"
+theme.layout_tilebottom = icons_path.."layouts/tilebottomw.png"
+theme.layout_tileleft   = icons_path.."layouts/tileleftw.png"
+theme.layout_tile = icons_path.."layouts/tilew.png"
+theme.layout_tiletop = icons_path.."layouts/tiletopw.png"
+theme.layout_spiral  = icons_path.."layouts/spiralw.png"
+theme.layout_dwindle = icons_path.."layouts/dwindlew.png"
+theme.layout_cornernw = icons_path.."layouts/cornernww.png"
+theme.layout_cornerne = icons_path.."layouts/cornernew.png"
+theme.layout_cornersw = icons_path.."layouts/cornersww.png"
+theme.layout_cornerse = icons_path.."layouts/cornersew.png"
 
-theme.ram_icon = assets_path.."icons/ram.png"
+theme.ram_icon = icons_path.."widgets/ram.png"
 theme.ram_fg = theme.white2
 theme.ram_high_fg = theme.yellow
 theme.ram_critical_fg = theme.red
 
-theme.network_sent_icon = assets_path.."icons/network-sent.png"
-theme.network_received_icon = assets_path.."icons/network-received.png"
+theme.network_sent_icon = icons_path.."widgets/network-sent.png"
+theme.network_received_icon = icons_path.."widgets/network-received.png"
 theme.network_fg = theme.white2
 theme.network_sent_fg = theme.blue
 theme.network_received_fg = theme.green
 
-theme.cpu_temperature_icon = assets_path.."icons/cpu-temperature.png"
+theme.cpu_temperature_icon = icons_path.."widgets/cpu-temperature.png"
 theme.cpu_temperature_fg = theme.white2
 theme.cpu_temperature_high_fg = theme.yellow
 theme.cpu_temperature_critical_fg = theme.red
